4bd150514100173cc2ca8fb63606bb94daf763b8
Henry Stanley <henry@henrystanley.com>
2014-11-10 17:43:04 +0000

    Updated using new tutorial - converting JAR to WAR

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -1,17 +1,16 @@
 package hello;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
-
+    
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
-        
     }
 }
